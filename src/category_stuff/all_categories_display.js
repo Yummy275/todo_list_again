c57b605deaf30eb36811aa35fb6778e52861ee72
Yummy275 <htgonzalez94@yahoo.com>
2019-12-29 11:41:56 -0700

    QS, cat buttons stacked next to category link

--- src/category_stuff/all_categories_display.js
+++ src/category_stuff/all_categories_display.js
@@ -5,25 +5,36 @@ import {specificCategoryLink} from "./button_functions"
 import {deleteCat} from "./button_functions"
 
 const categoriesDisplay = () => {
-  const catDiv = magicDOMTools.createAddtextAddclasses('div', "", 'catDiv');
+  const categoriesDisplayDiv = magicDOMTools.createAddtextAddclasses('div', "", 'allCatsDiv');
 
   /* Makes create button on top */
-  const createbtn = magicDOMTools.createAddtextAddclasses("button", "Create", "createBtn");
-  magicDOMTools.setEventListenerClick(createbtn , createCategory);
-  magicDOMTools.appendChildren(catDiv, createbtn);
+  const createBtn = magicDOMTools.createAddtextAddclasses("button", "Create", "createBtn");
+  magicDOMTools.setEventListenerClick(createBtn , createCategory);
+  magicDOMTools.appendChildren(categoriesDisplayDiv, createBtn);
 
   /*Print out categories of todo's*/
   for (var i = 0; i < categoriesHolder.container.length; i++) {
+    const categoryDiv = magicDOMTools.createAddtextAddclasses('div', "", 'categoryDiv')
+
     const categoryLink = magicDOMTools.createAddtextAddclasses('button', categoriesHolder.container[i].title,
       'categoryLink');
     categoryLink.addEventListener("click", specificCategoryLink.bind(event, categoriesHolder.container[i]));
+
+    const catEditDeleteBtnDiv = magicDOMTools.createAddtextAddclasses("div", "", "catEditDeleteBtnDiv")
+
     const editBtn = magicDOMTools.createAddtextAddclasses("button", "Edit", "catBtn");
+    /*edit function */
+
     const deleteBtn = magicDOMTools.createAddtextAddclasses("button", "Delete", "catBtn");
     deleteBtn.addEventListener("click", deleteCat.bind(event, categoriesHolder.container[i]));
-    magicDOMTools.appendChildren(catDiv, categoryLink, editBtn, deleteBtn);
+
+    magicDOMTools.appendChildren(catEditDeleteBtnDiv, editBtn, deleteBtn);
+
+    magicDOMTools.appendChildren(categoryDiv, categoryLink, catEditDeleteBtnDiv);
+    magicDOMTools.appendChildren(categoriesDisplayDiv, categoryDiv)
   }
 
-  magicDOMTools.appendToDisplayContent(catDiv);
+  magicDOMTools.appendToDisplayContent(categoriesDisplayDiv);
 }
 
 export{categoriesDisplay}
